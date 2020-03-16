ded9d988bfc22aef575c274dcd82c7f616f9c2da
carlynicholson <carlypnicholson@gmail.com>
2020-03-15 19:03:39 -0500

    updated JSON

--- app.js
+++ app.js
@@ -1,3 +1,83 @@
 $(".resToggle").click(function() {
   $(".myTopNav").toggleClass("active");
 });
+
+let url = 'https://docs.google.com/spreadsheets/d/193Hou07FkHfuSH9IccJxFxPHvxJdQwwb33kaxianMd4/edit?usp=sharing'
+let id = '193Hou07FkHfuSH9IccJxFxPHvxJdQwwb33kaxianMd4'
+let source = `https://spreadsheets.google.com/feeds/list/193Hou07FkHfuSH9IccJxFxPHvxJdQwwb33kaxianMd4/od6/public/values?alt=json`
+// let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+
+fetch(source)
+  .then( response => response.json() ) 
+  .then( data =>  {
+      let projects = data.feed.entry.map( project => {
+        return {
+          title: project.gsx$title.$t,
+          image: project.gsx$image.$t,
+          description: project.gsx$description.$t,
+          url: project.gsx$url.$t
+        }
+      })
+      
+      app(projects)
+  })
+
+function app(projects) {
+  console.log(projects)
+  projects.map((project,i) => {
+    const $ul = $("<ul>").addClass("deck");
+      $("deck").append($item);
+    let $item = $("<li>").addClass("card").url(`${project.url}`);
+      $("card").append($image);
+    let $image = $("<div>").addClass("card-image").css("background-image", `${project.image}`);
+      $("card-image").append($button);
+    let $button = $("<button>").addClass("card-btn btn-block").text(`Project ${i}`);
+      $("card-btn btn-block").append($content);
+    let $content = $("<div>").addClass("card-content");
+      $("card-content").append($title);
+    let $title = $("<div>").addClass("card-title").text(`${project.title}`);
+      $("card-title").append($text);
+    let $text = $("<p>").addClass("card-text").text(`${project.description}`);
+      $("card-text").append("card-content");
+  })
+}
+
+/*
+function app(projects) {
+  console.log(projects)
+  projects.map((project,i) => {
+    let $item = $("<li>").addClass("card").url(`${project.url}`);
+    let $image = $("<div>").addClass("card-image").css("background-image", `${project.image}`);
+    let $button = $("<button>").addClass("card-btn btn-block").text(`Project ${i}`);
+    let $content = $("<div>").addClass("card-content");
+    let $title = $("<div>").addClass("card-title").text(`${project.title}`);
+    let $text = $("<p>").addClass("card-text").text(`${project.description}`);
+  })
+}
+
+const $ul = $("<ul>").addClass("deck");
+    $("deck").append($li);
+
+const $li = $("<li>").addClass("card").url(`${project.url}`);
+    $("card").append($div);
+
+const $div = $("<div>").addClass("card-image").css("background-image", `${project.image}`);
+    $("card-image").append($button);
+
+const $button = $("<button>").addClass("card-btn btn-block").text(`Project ${i}`);
+    $("card-btn btn-block").append($div);
+
+const $div = $("<div>").addClass("card-content");
+    $("card-content").append($div);
+
+const $div = $("<div>").addClass("card-title").text(`${project.title}`);
+    $("card-title").append($p);
+
+const $p = $("<p>").addClass("card-text").text(`${project.description}`);
+*/    
+
+
+
+ /* for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+  } */
