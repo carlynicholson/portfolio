2b564c7539eee2c4a1eefed552409b2cbdb52d02
carlynicholson <carlypnicholson@gmail.com>
2023-02-13 23:10:57 -0600

    updates

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ let source = 'https://docs.google.com/spreadsheets/d/193Hou07FkHfuSH9IccJxFxPHvx
 const isHome = window.location.pathname === "/portfolio/" || window.location.pathname === "/index.html"
 
 fetch(source)
-.then(response => response.body()) 
+// .then(response => response.body()) 
   // .then(data => data.substring(47).slice(0, -2))
-  // .then(response => response.json())
+  .then(response => response.json())
   /*.then(data =>  data.feed.entry.map(project => ( {
           title: project.gsx$title.$t,
           image: project.gsx$image.$t,
